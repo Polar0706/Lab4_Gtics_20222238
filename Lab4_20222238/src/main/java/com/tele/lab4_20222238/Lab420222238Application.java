cf714eab3602b2339e154dcfd5558f5c986fb85d
Polar0706 <a20222238@pucp.edu.pe>
2025-05-09 20:31:16 -0500

    creacion proyecto

---
+++ Lab4_20222238/src/main/java/com/tele/lab4_20222238/Lab420222238Application.java
@@ -0,0 +1,13 @@
+package com.tele.lab4_20222238;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab420222238Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab420222238Application.class, args);
+    }
+
+}
---
+++ Lab4_20222238/src/test/java/com/tele/lab4_20222238/Lab420222238ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tele.lab4_20222238;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab420222238ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
